dc1fa8464de10c62440a8cd34035f3f05d5ba92d
Aditya Sriramteja Chilukuri <adityasriramteja@gmail.com>
2026-03-15 22:21:39 -0400

    Add Firebase hosting configuration and environment variables

--- src/lib/Firebase.ts
+++ src/lib/Firebase.ts
@@ -2,20 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBLVyHl3fYUV38W4B8LKetspawaZV4cOBc",
-  authDomain: "finding-maya.firebaseapp.com",
-  projectId: "finding-maya",
-  storageBucket: "finding-maya.firebasestorage.app",
-  messagingSenderId: "21248969890",
-  appId: "1:21248969890:web:990fb5ff610dfc679adbfb"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+const analytics = getAnalytics(app);
